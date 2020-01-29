8f9d5aece1020f75240098996c2ede4e02f72d63
bengrunfeld <benjamin.grunfeld@gmail.com>
2020-01-28 21:36:08 -0500

    Set up and test connection to database

---
+++ pages/api/current-game.js
@@ -0,0 +1,12 @@
+import sqlQuery from "../../lib/db";
+import escape from "sql-template-strings";
+
+const fetchCurrentGame = async (req, res) => {
+  const query = escape`SELECT * FROM current_game ORDER BY id`;
+
+  const currentGame = await sqlQuery(query);
+
+  res.status(200).json({ currentGame });
+};
+
+export default fetchCurrentGame;
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import Head from "next/head";
 import fetch from "isomorphic-unfetch";
 
-const Home = () => (
+const Home = ({ data }) => (
   <div>
     <Head>
-      <title>
-        485 Visa Insurance | Compare Multiple Providers In One Place
-      </title>
+      <title>The Button Game</title>
       <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width, user-scalable=no" />
     </Head>
 
-    <h1>Test</h1>
+    <h1>Last Pressed: {data.currentGame[0].last_pressed}</h1>
   </div>
 );
 
@@ -24,16 +22,10 @@ const getProtocol = host => {
 
 Home.getInitialProps = async ({ req, query }) => {
   const host = ((req || {}).headers || {}).host;
-
-  const safetyHost = host || "https://485visainsurance.com.au";
-
+  const safetyHost = host || "https://buttongame.com";
   const protocol = getProtocol(safetyHost);
 
-  const table = query.table || "budget_hospital_family";
-
-  const res = await fetch(
-    `${protocol}/api/products/485-visa-health-insurance/${table}`
-  );
+  const res = await fetch(`${protocol}/api/current-game`);
   const data = await res.json();
 
   return { data };
