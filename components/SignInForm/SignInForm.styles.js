7f0e6d3452da6a29211118fb44247167e7196a1c
bengrunfeld <benjamin.grunfeld@gmail.com>
2020-02-03 18:13:42 -0500

    Remove form test code

--- components/SignInForm/SignInForm.styles.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const SignInContainer = styled.div`
-  display: flex;
-`;
--- components/index.js
+++ components/index.js
@@ -1,4 +1,3 @@
 export { GameScreen } from "./GameScreen";
 export { HomePage } from "./HomePage";
-export { SignInForm } from "./SignInForm";
 export { NotSupported } from "./NotSupported";
